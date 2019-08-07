e299de0fb579792f36fc31b73c2d5fa7e9b97230
Chris Haining <chrishaining@yahoo.co.uk>
2019-08-07 13:51:18 +0100

    Just to test that forking is working

--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -9,23 +9,20 @@ class CustomerTest < MiniTest::Test
 def setup
   @drink1 = Drink.new("drink1", 1.50)
   @drink2 = Drink.new("drink2", 2.50)
-  @drink3 = Drink.new("drink3", 5)
+  @drink3 = Drink.new("drink3", 5.00)
 
   @pub1 = Pub.new("Jolly Farmer", 1000, [@drink1, @drink2, @drink3])
 
   @customer1 = Customer.new("Ted", 100)
 end
 
-
 def test_customer_name
   assert_equal("Ted", @customer1.name())
 end
 
-
 def test_customer_wallet
-  assert_equal(100, @customer1.wallet())
+  assert_equal(100.00, @customer1.wallet())
 end
-
 
 def test_buy_drink_from_pub
   @customer1.buy_drink_from_pub(@pub1)
@@ -34,7 +31,9 @@ end
 
 def test_reduce_value_of_wallet
   @customer1.reduce_value_of_wallet(@drink3.price)
-  assert_equal(95, @customer1.wallet())
+  assert_equal(95.00, @customer1.wallet())
 end
+
+
 
 end
