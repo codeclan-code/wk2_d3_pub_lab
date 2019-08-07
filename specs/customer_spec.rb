a6509289af90797d79d377f50e5a35075f8920cd
Ted Osuch <ted@osuch.com>
2019-08-07 12:10:15 +0100

    Completed Spec & Ruby Code

--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -4,8 +4,24 @@ require_relative('../customer')
 
 class CustomerTest < MiniTest::Test
 
-# def setup
-#
-# end
+def setup
+  @customer1 = Customer.new("Ted", 100)
+end
+
+
+def test_customer_name
+  assert_equal("Ted", @customer1.name())
+end
+
+
+def test_customer_wallet
+  assert_equal(100, @customer1.wallet())
+end
+
+
+
+
+
+
 
 end
